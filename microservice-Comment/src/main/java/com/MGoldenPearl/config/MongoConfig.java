3054104c31c053331008d98daa1c06cf93d90539
Edu-Reyna <eduardor1901g@gmail.com>
2025-10-29 22:25:31 -0500

    test CRUD comment microservice

---
+++ microservice-Comment/src/main/java/com/MGoldenPearl/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.MGoldenPearl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
